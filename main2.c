9b5cdef30da301c01440050d90f1add60f92b6c0
Fuzzy <rodrigo.rosa.005@acad.pucrs.br>
2018-05-06 21:32:22 -0300

    Update main2.c

--- main2.c
+++ main2.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>		// Para usar strings
+#include <errno.h>   // para errno
+#include <limits.h>  // para o max int
 
 // SOIL é a biblioteca para leitura das imagens
 #include "SOIL.h"
@@ -27,7 +29,9 @@ typedef struct {
 void load(char* name, Img* pic);
 void gravaBitPalavra(int cor,int pixelPos,Img pic, unsigned char w,  unsigned char ww);
 char encript(char* texto, int chave, int tipo);
-
+void steganography (Img pic, char* frase);
+void undoGraphy (Img pic, int chave);
+getBit (int cor,int pixelPos,Img pic, int position);
 
 
 
@@ -81,8 +85,15 @@ char encript(char* texto, int chave, int tipo) // tipo = 0 -> cifrar| tipo = 1 -
                     else
                         letra++;
 
-                    if(cont == tam)
+                    if(cont == tam){
                         break;
+                    }
+                    if(texto[i] == FINALCHAR){
+
+                        break;
+
+                    }
+
 
                 }
         }
@@ -106,7 +117,7 @@ void load(char* name, Img* pic)
         printf( "SOIL loading error: '%s'\n", SOIL_last_result() );
         exit(1);
     }
-    printf("Load: %d x %d x %d\n", pic->width, pic->height, chan);
+    printf("Imagem carregada: %d x %d x %d %s\n", pic->width, pic->height, chan,name);
 }
 
 
@@ -130,102 +141,39 @@ void gravaBitPalavra(int cor,int pixelPos,Img pic, unsigned char w,  unsigned ch
     return 0;
 }
 
-
-int main(int argc, char** argv)
-{
-    Img pic;
+void steganography (Img pic, char* frase){
     int cor = 0 ;
     int pixelPos = 0 ;
-
-
-
-
-    if(argc < 2) {
-        printf("loader [img]\n");
-        exit(1);
-    }
-
-    load(argv[1], &pic);
-
-    char* frase  = argv[2];
-    encript(frase, 0, 0);
-
     unsigned char w;
     unsigned char ww;
     int a=0;
     int b=0;
     int tam=strlen(frase);
    // int tam=1;
-    printf("Bits de cada palavra:\n");
+    printf("Iniciando...\n");
     while(a<=tam){
 
 
 
-        printf("%c",frase[a]);
-        printf("%s"," -> ");
-        for(b = 7; 0 <= b; b--){
-                printf("%d",(frase[a] >> b) & 0x01);
-            }
-            printf("\n");
         for(b = 7; 0 <= b; b-=2){
-             w = ((frase[a] >> b) & 0x01); //pega o bit na posicao b e faz um AND com o bit 1 retorna um se
-                                                      //forem iguais ou seja se b for 1 e retorna 0 se forem diferentes
-                                                      //ou seja quando b for zero
+             w = ((frase[a] >> b) & 0x01);
 
              ww = (frase[a] >> b-1) & 0x01;
-             printf("\n");
-             printf("%s%d","Valor de W: ",w);
-             printf("\n");
-             printf("%s%d","Valor de WW: ",ww);
-             printf("\n");
+
             if (cor >= 3){
                 pixelPos ++;
                 cor=0;
             }
-
-            printf("%s%d%s%d\n","COR ",cor,"  PIXEL ",pixelPos);
-            printf("%s\n","antes : ");
-            for(int d = 7; 0 <= d; d--){
-                printf("%d",(pic.img[pixelPos].r >> d) & 0x01);
-            }
-            printf("\n");
-            for(int r = 7; 0 <= r; r--){
-                printf("%d",(pic.img[pixelPos].g >> r) & 0x01);
-            }
-            printf("\n");
-            for(int g = 7; 0 <= g; g--){
-                printf("%d",(pic.img[pixelPos].b >> g) & 0x01);
-            }
-            printf("\n");
-
-
             gravaBitPalavra(cor,pixelPos,pic, w, ww);
             cor ++;
-
-            printf("%s\n","depois: ");
-
-            for(int h = 7; 0 <= h; h--){
-                printf("%d",(pic.img[pixelPos].r >> h) & 0x01);
-            }
-            printf("\n");
-            for(int n = 7; 0 <= n; n--){
-                printf("%d",(pic.img[pixelPos].g >> n) & 0x01);
-            }
-            printf("\n");
-            for(int m = 7; 0 <= m; m--){
-                printf("%d",(pic.img[pixelPos].b >> m) & 0x01);
-            }
-
         }
-        printf("\n");
-        printf("%s\n","FIM");
         a++;
     }
-
-    //grava o char final para marcar o final da mensagem gravada na imagem
-    for(int aa = 7; 0 <= aa; aa--){
-            printf("%d",(FINALCHAR >> aa) & 0x01);
-    }
+        for(int d = 7; 0 <= d; d--){
+             printf("%d",(pic.img[0].r >> d) & 0x01);
+        }
+    //Grava o char final para marcar o final da mensagem gravada na imagem
+    printf("%s\n","Gravando simbolo final...");
     for(b = 7; 0 <= b; b-=2){
         if (cor >= 3){
             pixelPos ++;
@@ -236,19 +184,93 @@ int main(int argc, char** argv)
         gravaBitPalavra(cor,pixelPos,pic, w, ww);
         cor ++;
     }
-
-
-
-
-
-
-
     printf("\n");
     SOIL_save_image("saida.bmp", SOIL_SAVE_TYPE_BMP, pic.width,
                     pic.height, 3, pic.img);
     free(pic.img);
+
 }
 
+int getBit (int cor,int pixelPos,Img pic, int position){
+    int bit =0;
+  switch (cor){
+            case 0:
+                bit = (pic.img[pixelPos].r >> position) & 0x01;
+                break;
+            case 1:
+                bit = (pic.img[pixelPos].g >> position) & 0x01;
+                break;
+            case 2:
+                bit = (pic.img[pixelPos].b >> position) & 0x01;
+                break;
+        }
+        return bit;
+
+}
+
+void undoGraphy (Img pic, int chave){
+
+    unsigned char frase[300];
+    unsigned char ch;
+    int cor= 0;
+    int pixelPos=0;
+    for (int a=0;  a<pic.height * pic.width; a++){
+        ch = 0x00;
+        for(int b = 7; 0 <= b; b-=2){
+                if (cor >= 3){
+                    pixelPos ++;
+                    cor=0;
+                }
+
+                 ch ^= (-(getBit( cor,pixelPos,pic,1)) ^ ch) & (0x01 << b);
+                 ch ^= (-(getBit( cor,pixelPos,pic,0)) ^ ch) & (0x01 << b - 1);
+                 cor ++;
+            }
+            if (ch == FINALCHAR){
+                frase[a] = ch;
+                break;
+            }
+            frase[a] = ch;
+    }
+    encript(frase,chave, 1);
+
+
+
+}
+
+int main(int argc, char** argv)
+{
+    Img pic;
+    char* p;
+
+    if(argc < 2) {
+        printf("loader ERRO [img]\n");
+        exit(1);
+    }
+
+    load(argv[1], &pic);
+    int chave = strtol(argv[3], &p, 10);
+
+    if (errno != 0 || *p != '\0' || chave > INT_MAX) {
+            printf("%s", "ERRO: Chave deve ser um numero inteiro.");
+            return 0;
+    }
+
+    if (strcmp(argv[2], "-hide") == 0){
+        char* frase  = argv[4];
+
+        encript(frase, chave, 0);
+        steganography(pic,frase);
+
+    }else{
+        if (strcmp(argv[2], "-reveal") == 0){
+            undoGraphy (pic, chave);
+        }else{
+            printf("%s", "ERRO: Argumento 2 não reconhecido use -hide ou -reveal.");
+        }
+    }
+
+}
 
 
 
